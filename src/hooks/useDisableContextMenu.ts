6f09d7fd3918a6b142b2a3b19bb0745f9784cdfa
shahadathakhand747 <shahadathakhand747@gmail.com>
2026-04-19 18:43:00 +0600

    Remove old scaffold: src/hooks/useDisableContextMenu.ts

--- src/hooks/useDisableContextMenu.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-
-export function useDisableContextMenu() {
-  useEffect(() => {
-    const handler = (e: MouseEvent) => e.preventDefault();
-    document.addEventListener('contextmenu', handler);
-    return () => document.removeEventListener('contextmenu', handler);
-  }, []);
-}
